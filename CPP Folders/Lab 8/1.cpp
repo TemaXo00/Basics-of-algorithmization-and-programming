8ba1b72092a0c5c556e0e73d0cdbc109a8c4a965
SehtQurq <melnikov.artem294@gmail.com>
2025-01-17 14:53:26 +0300

    Updated Lab 8, added photos and refresh task

--- CPP Folders/Lab 8/1.cpp
+++ CPP Folders/Lab 8/1.cpp
@@ -4,39 +4,36 @@
 #define space text[i]==' '
 using namespace std;
 
-void contPr() {
+void continueProgram() {
     cout << "\nНажмите Enter для продолжения...";
     cin.get();
     system("clear");
 }
-int getCountOf (string text, int idOfSearch, int endOfSearch) {
+int getCountOf(string text, int begin, int end, char id) {
     int counter=0;
-    // 1 - пробелы, для количества слов, 2 - для поиска количества предложений
-    for (int i=0; i<endOfSearch; i++) {
-        switch(idOfSearch) {
-            case 1: if (space) counter++; break;
-            case 2: if (sentences) counter++; break;
+    for (int i=begin; i<end; i++) {
+        switch(id) {
+            case 'S': if (sentences) counter++; break;
+            case 'W': if (space) counter++; break;
             default: break;
         }
     }
     return counter;
 }
-void printNeeded(string text, int BOS, int EOS, int NOS, int neededId) {
-    //id=1 - для предложений, id=2 - для слов
-    cout << "Вывод " << (neededId==1 ? "предложения №": "слова №") << NOS << ": ";
-    for (int i=BOS; i<=EOS; i++)
+bool checkForWorkplace(string text) {
+    int counter= getCountOf(text, 0, text.length(), 'S');
+    if (counter<=1)
+        return false;
+    else
+        return true;
+}
+void print(string text, int begin, int end, int number, char id) {
+    cout << (id=='S' ? "Вывод предложения №": "Вывод слова №") << number << ": ";
+    for (int i=begin; i<=end; i++)
         cout << text[i];
     cout << endl;
 }
-
-bool infoAS(string text) {
-    bool info=false;
-    int searchS= getCountOf(text,2,text.length());
-    if (searchS!=0)
-        info=true;
-    return info;
-}
-string convertToFift(string number) {
+void convertToFift(string number) {
     string fifteen[15]={"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E"};
     cout << "Первое число в первом предложении: " << number << endl;
     int myNum = stoi(number);
@@ -46,9 +43,7 @@ string convertToFift(string number) {
         myNum/=15;
     } while(myNum>0);
     cout << "Переведенный в пятнадцатиричную систему: " << number << endl;
-    return number;
 }
-
 int main() {
     string text;
     int task;
@@ -59,103 +54,93 @@ int main() {
         switch(task) {
             case 1: cout << "Введите текст:\n"; cin.ignore(); getline(cin, text); break;
             case 2: text="The parade of hundreds of demons was held in Japan on December 24, 2017.The main figure was the special rank shaman Suguru Geto, who managed more than 2000 curses.Stop it was only through the efforts of student 1 year of the Magic College Yuta Okkotsu, who with a curse of special rank defeated Geto.During the incident, about 183 people were injured, 546 were wounded.";
-            cout << "Текст из примера был взят успешно"; break;
+                cout << "Текст из примера был взят успешно"; cin.ignore(); break;
             default: cout << "Произошла ошибка. Вы ввели неправильный номер задания\n";
         }
     } while(task<1 || task>2);
-    bool SC = infoAS(text);
-    if (!SC) {
-        cout << "В тексте не было найдено ни одного предложения. Закрываем программу...";
+    text.insert(0,".");
+    continueProgram();
+    bool checkForWork = checkForWorkplace(text);
+    if (!checkForWork) {
+        cout << "В тексте не было найдено ни одного предложения. Программа не может отработать.";
         return 0;
     }
-    cin.ignore();
-    contPr();
-    //1.по нажатию произвольной клавиши поочередно выделяет каждое
-    //предложение текста, выводит его, а также определяет количество предложений в
-    //тексте;
-    int COS = getCountOf(text, 2, text.length()), dots[COS+1], counter=1;
+    //Task 1
+    int sentencesCounter = getCountOf(text,0,text.length(),'S');
+    int dotsPlacement[sentencesCounter], counter=0;
     for (int i=0; i<text.length(); i++) {
         if (sentences) {
-            dots[counter]=i;
+            dotsPlacement[counter]=i;
             counter++;
         }
     }
-    dots[0]=0;
-    printNeeded(text, dots[0], dots[1], 1, 1);
-    for (int i=1; i<COS; i++)
-        printNeeded(text, dots[i] + 1, dots[i + 1], i + 1, 1);
-    printf("Количество предложений равно: %d",COS);
-    contPr();
-    //2.определяет количество слов в первом предложении и выводит каждое
-    //четное слово;
-    int countOfWords = getCountOf(text,1,dots[1])+1;
-    printf("Количество слов в первом предложении равно: %d\n",countOfWords);
+    for (int i=0; i<sentencesCounter-1; i++)
+        print(text,dotsPlacement[i]+1,dotsPlacement[i+1], i+1, 'S');
+    printf("Количество предложений в тексте: %d",sentencesCounter-1);
+    continueProgram();
+    //Task 2
+    int wordsCounter = getCountOf(text,dotsPlacement[0],dotsPlacement[1],'W')+1;
+    counter=0;
+    printf("Количество слов: %d\n", wordsCounter);
     struct words {
         int begin;
         int end;
         int series;
     };
-    words wd[countOfWords];
-    counter=1;
-    wd[0].begin=0;
-    wd[countOfWords-1].end=dots[1]-1;
-    for (int i=0; i<dots[1]; i++) {
+    words w[wordsCounter];
+    w[0].begin=dotsPlacement[0]+1;
+    w[wordsCounter-1].end=dotsPlacement[1]-1;
+    for (int i=dotsPlacement[0]; i<dotsPlacement[1]; i++) {
         if (space) {
-            wd[counter-1].end=i-1;
-            wd[counter].begin=i+1;
+            w[counter].end=i-1;
+            w[counter+1].begin=i+1;
             counter++;
         }
     }
-    for (int i=1; i<=countOfWords; i+=2)
-        printNeeded(text, wd[i].begin, wd[i].end, i+1,2);
-    contPr();
-    //3.переводит первое целое число первого предложения из десятичной
-    //системы счисления в пятнадцатеричную.
+    for (int i=1; i<wordsCounter; i+=2)
+        print(text,w[i].begin,w[i].end,i+1,'W');
+    continueProgram();
+    //Task 3
     string number;
-    for (int i=0; i<countOfWords; i++) {
-        for (int j=wd[i].begin; j<=wd[i].end; j++) {
-            if (isdigit(text[j])) {
+    bool foundedFirst=false;
+    for (int i=0; i<wordsCounter; i++) {
+        for (int j=w[i].begin; j<=w[i].end; j++) {
+            if (isdigit(text[j]) && !foundedFirst)
                 number+=text[j];
-            }
         }
-        if (number!="")
-            break;
+        if (!number.empty())
+            foundedFirst=true;
     }
-    if (number=="")
-        cout << "В первом предложении не найдено ни одного числа.";
+    if (!foundedFirst)
+        cout << "В первом предложении число не было найдено";
+    else
+        convertToFift(number);
+
+    continueProgram();
+    //Task 4
+    bool three = sentencesCounter>4;
+    if (!three)
+        cout << "В тексте менее трёх предложений";
     else {
-        number = convertToFift(number);
+        for (int i=3; i>0; i--)
+            print(text,dotsPlacement[i-1]+1,dotsPlacement[i],i,'S');
     }
-    contPr();
-    //4.каждое из первых трех предложений текста выводит в обратном порядке;
-    bool threeSent = COS>3;
-    if (!threeSent)
-        cout << "В тексте не было найдено три предложения";
-    else {
-        for (int i = 4; i > 2; i--)
-            printNeeded(text,dots[i-1]+1, dots[i], i-1, 1);
-        printNeeded(text, dots[0], dots[1], 1, 1);
-    }
-    contPr();
-    //5.упорядочивает слова первого предложения в соответствии с ростом их
-    //характеристик (характеристика - длина содержащейся в слове максимальной
-    //серии);
-    for (int i=0; i<countOfWords; i++) {
-        wd[i].series = (wd[i].end-wd[i].begin)+1;
-    }
-    for (int i=0; i<countOfWords; i++) {
-        for (int j=0; j<countOfWords-1; j++) {
-            if (wd[j].series>wd[j+1].series) {
-                swap (wd[j].begin, wd[j+1].begin);
-                swap (wd[j].end, wd[j+1].end);
-                swap (wd[j].series, wd[j+1].series);
+    continueProgram();
+    //Task 5
+    for (int i=0; i<wordsCounter; i++)
+        w[i].series=(w[i].end-w[i].begin)+1;
+    for (int i=0; i<wordsCounter; i++) {
+        for (int j=0; j<wordsCounter-1; j++) {
+            if (w[j].series>w[j+1].series) {
+                swap (w[j].begin, w[j+1].begin);
+                swap (w[j].end, w[j+1].end);
+                swap (w[j].series, w[j+1].series);
             }
         }
     }
     cout << "Отсортированное по серии первое предложение:\n";
-    for (int i=0; i<countOfWords; i++) {
-        printNeeded(text, wd[i].begin, wd[i].end, i+1,2);
-    }
-    contPr();
-    cout << "Программа завершена успешно.";
+    for (int i=0; i<wordsCounter; i++)
+        print(text, w[i].begin, w[i].end,i+1,'W');
+    continueProgram();
+    cout << "Программа была завершена успешно.";
 }
--- Projects Folders/Lab 8/Task_1/cmake-build-debug/cmake_install.cmake
+++ Projects Folders/Lab 8/Task_1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/Media/Works/Labs/MITSO-1 Course. ОАИП. ЛР Re/Lab 8/Task_1
+# Install script for directory: /home/seht/Works/Labs/MITSO-1 Course. ОАИП. ЛР/Lab 8/Task_1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -57,6 +57,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/mnt/Media/Works/Labs/MITSO-1 Course. ОАИП. ЛР Re/Lab 8/Task_1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/seht/Works/Labs/MITSO-1 Course. ОАИП. ЛР/Lab 8/Task_1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
